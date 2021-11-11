7a9a7fb7067190991ee88a3a11041fd07193aeaf
jaspreet09915 <jaspreetsingh09915@gmail.com>
2021-11-11 17:30:37 +0530

    defining the inherited interface method

--- CWH_58_inheritance_in_interface.java
+++ CWH_58_inheritance_in_interface.java
@@ -12,3 +12,18 @@ interface childSampleInterface extends sampleInterface{
 }
 public class CWH_58_inheritance_in_interface {
 }
+
+class MySampleInterface implements childSampleInterface{
+    public void meth1(){
+        System.out.println("Meth1");
+    }
+    public void meth2(){
+        System.out.println("Meth2");
+    }
+    public void meth3(){
+        System.out.println("Meth3");
+    }
+    public void meth4(){
+        System.out.println("Meth4");
+    }
+}
