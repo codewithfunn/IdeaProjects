7a30f41c7301451c98a7c4f0e9bbbf73ee0d00fa
jaspreet09915 <jaspreetsingh09915@gmail.com>
2021-11-14 03:09:53 +0530

    no need to import Scanner package just provide the full path where the package reside

--- CWH_65_packages.java
+++ CWH_65_packages.java
@@ -1,9 +1,11 @@
 package com.company;
 //import java.util.Scanner;
-import java.util.*;
+//import java.util.*;
 public class CWH_65_packages {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
+        // providing the full path where the Scanner class reside
+        java.util.Scanner sc = new java.util.Scanner(System.in);
         System.out.println("This is my scanner class");
     }
 }
