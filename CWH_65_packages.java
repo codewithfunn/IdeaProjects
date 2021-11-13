1f6d882129440168a077d2485b1499214fc22e3a
jaspreet09915 <jaspreetsingh09915@gmail.com>
2021-11-14 03:02:35 +0530

    import java.util.* means import all class within util folder

--- CWH_65_packages.java
+++ CWH_65_packages.java
@@ -1,6 +1,6 @@
 package com.company;
-import java.util.Scanner;
-
+//import java.util.Scanner;
+import java.util.*;
 public class CWH_65_packages {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
