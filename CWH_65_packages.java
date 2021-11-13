2c60632c7c34a2cb0aad987e01992b5753825088
jaspreet09915 <jaspreetsingh09915@gmail.com>
2021-11-14 03:00:39 +0530

    import Scanner (build-in) package

---
+++ CWH_65_packages.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.util.Scanner;
+
+public class CWH_65_packages {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("This is my scanner class");
+    }
+}
