8ed24f1429dcb6a1cd0ce7de4b74e3e75888e6ca
jaspreet09915 <jaspreetsingh09915@gmail.com>
2021-11-14 21:05:08 +0530

    we can't use private modifier within a package

--- CWH_66_Access_modifier.java
+++ CWH_66_Access_modifier.java
@@ -17,5 +17,9 @@ public class CWH_66_Access_modifier {
     public static void main(String[] args) {
         C1 c = new C1();
         c.meth1();
+        System.out.println(c.x);
+        System.out.println(c.y);
+        System.out.println(c.z);
+      // System.out.println(c.a); -> through an error due to access modifiers
     }
 }
