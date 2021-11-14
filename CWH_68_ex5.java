f529085d015ca9f389e07a5adce015b4b4e44214
jaspreet09915 <jaspreetsingh09915@gmail.com>
2021-11-15 00:03:43 +0530

    exercises 5 solved using notepad

---
+++ CWH_68_ex5.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class CWH_68_ex5 {
+    public static void main(String[] args){
+        /*
+        ********Write this code in notepad*******
+        * You have to create a package named com.codewithharry.shape
+        * This package should have individual classes for Rectangle , Square , Circle , Cylinder , Sphere
+        * These classes should use inheritance to property managed the code!
+        * Include methods like volume , surface area and getter/setters for dimensions*/
+    }
+
+}
