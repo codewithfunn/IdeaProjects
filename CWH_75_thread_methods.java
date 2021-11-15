5bbc57c18af3b6f0f1f55dcc95f50c97ce2cf63f
jaspreet09915 <jaspreetsingh09915@gmail.com>
2021-11-15 06:59:38 +0530

    The join() method desc

--- CWH_75_thread_methods.java
+++ CWH_75_thread_methods.java
@@ -23,6 +23,14 @@ public class CWH_75_thread_methods {
         MyTh1 t1 = new MyTh1();
         MyTh2 t2 = new MyTh2();
         t1.start();
+        /*
+        Join() method In Java :
+    1.  The join()method in Java allows one thread to wait until the execution of some other specified
+        thread is completed.
+    2.  If t is a Thread object whose thread is currently executing, then t.join() causes
+        the current thread to pause execution until t's thread terminates.
+    3.  Join() method puts the current thread on wait until the thread on which it is called is dead.
+     */
         try {
             t1.join();
         } catch (Exception e) {
