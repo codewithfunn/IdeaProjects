26b7dce2173701609ea3cd28e9f071c4e6982e9e
jaspreet09915 <jaspreetsingh09915@gmail.com>
2021-11-15 07:50:46 +0530

    solving problem 3

--- CWH_76_practiceSet13.java
+++ CWH_76_practiceSet13.java
@@ -15,11 +15,11 @@ class ThrSol1b extends Thread{
         while(j<500){
             System.out.println("Welcome!");
             j++;
-            try {
-                Thread.sleep(200);
-            }catch (Exception e){
-                System.out.println(e);
-            }
+//            try {
+//                Thread.sleep(200);
+//            }catch (Exception e){
+//                System.out.println(e);
+//            }
         }
     }
 }
@@ -28,8 +28,17 @@ public class CWH_76_practiceSet13 {
         // Write a program to print "good Morning" and "welcome" Continuously on the screen in java using Thread
         ThrSol1 t1 = new ThrSol1();
         ThrSol1b t2 = new ThrSol1b();
+//        t1.start();
+//        t2.start();
+        // Add a sleep method in welcome thread of question1 to delay its execution for 200ms
+
+        // Demonstrate getPriority() and setPriority() methods in java threads
+        t1.setPriority(7);
+        t2.setPriority(9);
+
+        System.out.println("the priority of t1 is " + t1.getPriority());
+        System.out.println("the priority of t2 is " + t2.getPriority());
         t1.start();
         t2.start();
-        // Write a sleep method in welcome thread of question1 to delay its execution for 200ms
     }
 }
