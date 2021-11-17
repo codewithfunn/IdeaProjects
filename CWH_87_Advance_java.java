177659437f0c8a6fe3dd12ceac8b2e66963430dc
jaspreet09915 <jaspreetsingh09915@gmail.com>
2021-11-17 19:59:13 +0530

    intro to advance java

---
+++ CWH_87_Advance_java.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/*
+    * Collection framework
+        A Collection represents a group of objects. Java collections provide classes and interfaces for us to be able
+        to write code quickly and efficiently
+
+    * Why do we need Collections?
+        We need Collections for efficient Storage and better manipulation of data in java.
+        * for example : we use arrays to store integer but what if we want to
+            * Resize this array?
+            * Insert an element in between?
+            * Delete an element in Array?
+            * Apply Certain operation to change this array
+
+    * How are Collection available
+       * Collection in java are available as Classes and interfaces. Following are few commonly used Collection in java
+            * ArrayList -> for variable size collection
+            * Set -> for distinct Collection
+            * Stack -> A LIFO data Structure
+            * HashMap -> for storing key-value pairs
+        Collection class is available in java.util package . Collection class also provide static methods for sorting
+        searching etc.
+       */
+public class CWH_87_Advance_java {
+}
