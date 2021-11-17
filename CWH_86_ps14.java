87aa33147485df98ef6576c7f6729af23366eb06
jaspreet09915 <jaspreetsingh09915@gmail.com>
2021-11-17 18:23:48 +0530

    problem 4 solved

--- CWH_86_ps14.java
+++ CWH_86_ps14.java
@@ -1,7 +1,14 @@
 package com.company;
 
 import java.util.Scanner;
-
+class MaxRetries extends Exception{
+    public String toString(){
+        return "Number of try is exceed to 5 ";
+    }
+    public String getMessage(){
+        return "Number of try is exceed to 5";
+    }
+}
 public class CWH_86_ps14 {
     public static void main(String[] args){
         // 1.  write a java program to demonstrate syntax, logical & runtime errors
@@ -57,7 +64,40 @@ public class CWH_86_ps14 {
                 System.out.println("Invalid index ");
                 i++;
             }
+            if(i>=5){
+                    System.out.println("Error");
+            }
+            }
 
+        // modify program in Q3 to throw a custom Exception if max retries are reached
+
+        int [] mark =new int[3];
+        mark[0] = 23;
+        mark[1] = 25;
+        mark[2] = 3;
+        int index ;
+        int j= 0;
+        boolean flag = true;
+
+        while(flag&&j<5){
+            try{
+                System.out.println("Enter the value of index ");
+                index = sc.nextInt();
+                System.out.println("the value at "+ index +" is "+ mark[index]);
+                flag = false;
+            }
+            catch (Exception e){
+                j++;
+                System.out.println("invalid index number");
+            }
         }
+        if(j<=6){
+            try{
+                throw new MaxRetries();
+            }catch (MaxRetries e){
+                System.out.println(e);
+            }
+        }
+
     }
 }
